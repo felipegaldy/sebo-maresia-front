7bc897eea36f8e7a609219324f317ee5f3370e1c
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-08-01 21:31:50 -0300

    carrosel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/estaticos/navbar/Navbar';
+import Carrosel from './components/carrosel/Carrosel';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Carrosel />
     </>
   );
 }
